eab9089e0e82dd18b48bd4b282bc7f0cbaa0f2d1
Vikas <victorjonathon@gmail.com>
2024-03-26 19:41:52 +0530

    Adding iterator to an object with generator function

--- es6-iterator-n-generator.js
+++ es6-iterator-n-generator.js
@@ -4,7 +4,7 @@ let obj = {
 }
 
 //Adding iterator to the object
-obj[Symbol.iterator] = function(){
+/*obj[Symbol.iterator] = function(){
     let objKeys = Object.keys(obj);
     let count = 0;
     let isDone = false;
@@ -16,6 +16,14 @@ obj[Symbol.iterator] = function(){
         return { done: isDone, value: this[objKeys[count++]]}
     }
     return { next };
+}*/
+
+//Adding iterator to the object with generator function
+obj[Symbol.iterator] = function*(){
+    let objKeys = Object.keys(obj);
+    for(let t of objKeys){
+        yield this[t];
+    }
 }
 
 //console.log(Object.values(obj));
