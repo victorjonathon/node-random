4b5bede5ee7deaaa82d166cf6743a2aab48a80f9
Vikas <victorjonathon@gmail.com>
2024-04-04 18:29:30 +0530

    Reading and writing from/to in chunks

---
+++ fs-chunk.js
@@ -0,0 +1,19 @@
+const fs = require('node:fs');
+
+const readStream = fs.createReadStream(
+    './static/chunk.txt',
+    {
+        encoding: 'utf-8'
+    }
+);
+
+const writeStream = fs.createWriteStream('./static/chunk2.txt');
+
+readStream.on('error', err => {
+    console.log(err.message);
+});
+
+readStream.on('data', (chunk) => {
+    console.log(chunk);
+    writeStream.write(chunk);
+});
